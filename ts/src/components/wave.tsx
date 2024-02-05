5d8a7c973c1de0659de34534a0283caab9d4ab46
Olek <149747215+Primis1@users.noreply.github.com>
2024-02-04 19:40:52 -0500

    hell yea

--- ts/src/components/wave.tsx
+++ ts/src/components/wave.tsx
@@ -5,7 +5,6 @@ export default function Wave() {
     return (
         <div className="ocean">
             <div className="wave"></div>
-            <div className="wave wave2"></div>
         </div>
     )
 }
